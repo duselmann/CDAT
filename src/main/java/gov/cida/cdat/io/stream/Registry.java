88dd338b3961e1664dc6cce7e869c87f8af26aa7
David Uselmann <duselmann@compcc.com>
2015-01-29 15:27:38 -0600

    intial attempt to monitor status - I dislike this but I wanted to document its iteration

--- src/main/java/gov/cida/cdat/io/stream/Registry.java
+++ src/main/java/gov/cida/cdat/io/stream/Registry.java
@@ -1,6 +1,9 @@
 package gov.cida.cdat.io.stream;
 
 
+import gov.cida.cdat.control.Status;
+
+import java.util.HashMap;
 import java.util.Map;
 import java.util.WeakHashMap;
 
@@ -9,37 +12,50 @@ import akka.actor.ActorRef;
 
 public class Registry {
 	
-	final Map<String,ActorRef> actors;
+	final Map<String,ActorRef> workers;
+	final Map<String,String>   stati;
 	
 	public Registry() {
 		// when objects get GC'ed they will not be held in this registry
-		actors = new WeakHashMap<String, ActorRef>();
+		workers = new WeakHashMap<String, ActorRef>();
+		stati   = new HashMap<String, String>();
 	}
 	
 	public ActorRef get(String name) {
-		return actors.get(name);
+		return workers.get(name);
 	}
 	
 	public String put(String name, ActorRef actor) {
 		String uniqueName = createUniqueName(name);
-		actors.put(uniqueName, actor);
+		workers.put(uniqueName, actor);
+		setStatus(name, Status.isNew);
+
 		return name;
 	}
 
-	// protected for testing access
-	protected String createUniqueName(String name) {
+	// package access for testing
+	String createUniqueName(String name) {
 		int count = 0; // internal count auto-named entries
 		
 		String uniqueName = name;
 		
-		while ( actors.containsKey(uniqueName) ) {
+		while ( workers.containsKey(uniqueName) || stati.containsKey(uniqueName) ) {
 			uniqueName = name + (count++);
 		}
 		
 		return uniqueName;
 	}
 
-//	public int size() {
-//		return actors.size();
-//	}
+	
+	public void setStatus(String name, String status) {
+		stati.remove(name);
+		stati.put(name, status);
+	}
+	public void setStatus(String name, Status status) {
+		if ( Status.isDone.equals(status) ) {
+			workers.remove(name);
+		}
+		setStatus(name, status.toString());
+	}
+
 }
