03fbe4a5ed48f687c4eaf99d4b1a88904242c824
David Uselmann <duselmann@usgs.gov>
2015-02-12 13:34:16 -0600

    Time helper methods

--- src/main/java/gov/cida/cdat/control/Time.java
+++ src/main/java/gov/cida/cdat/control/Time.java
@@ -10,4 +10,36 @@ public class Time {
 	public static final FiniteDuration MINUTE    = Duration.create(  1, "minute");
 	public static final FiniteDuration HOUR      = Duration.create(  1, "hour");
 	public static final FiniteDuration DAY       = Duration.create(  1, "day");
+	
+	/**
+	 * @return current time in milliseconds
+	 */
+	public static long now() {
+		return System.currentTimeMillis();
+	}
+	
+	/**
+	 * @param duration milliseconds to add to the current time
+	 * @return current time plus the given duration
+	 */
+	public static long later(long duration) {
+		return now() + duration;
+	}
+	
+	/**
+	 * convenience method for scala lib durations
+	 * @param duration an amount to add to the current time
+	 * @return the current time plue the given duration
+	 */
+	public static long later(FiniteDuration duration) {
+		return later(duration.toMillis());
+	}
+	
+	/**
+	 * @param timeInMilliseconds a time in milliseconds
+	 * @return now - the given time (a duration)
+	 */
+	public static long duration(long timeInMilliseconds) {
+		return now() - timeInMilliseconds;
+	}
 }
